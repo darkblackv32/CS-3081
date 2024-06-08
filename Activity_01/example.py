997da6022fb11d46ea954292400de14904a98b48
darkblackv32 <angello.soldi@protonmail.com>
2024-06-07 21:34:24 -0500

    f_com

--- Activity_01/example.py
+++ Activity_01/example.py
@@ -1,15 +1,3 @@
-# import requests
-
-
-# def get_coordinates(query):
-#     api_url = f"https://nominatim.openstreetmap.org/search?q={query}&format=json"
-#     headers = {"User-Agent": "my-app"}
-#     response = requests.get(api_url, headers=headers)
-#     response_data = response.json()
-#     # if not response_data:
-#         # return None
-#     return float(response_data[0]["lat"]), float(response_data[0]["lon"])
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import requests
